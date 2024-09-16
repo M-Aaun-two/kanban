cdf36f1b96ef9daaa6b425e9723b40b60cc45d07
Aaun <m.aauntariq@gmail.com>
2024-09-16 07:33:36 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { CustomKanban } from './section/kanban';
+import IconSideNav from './section/navbar';
+
+function App() {
+  return (
+    <main>
+      <IconSideNav />
+    </main>
+  );
+}
+
+export default App;
